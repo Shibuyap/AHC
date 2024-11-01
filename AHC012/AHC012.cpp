98477e06252c096816087217da4c559587b2732f
f-shibuya <f-shibuya@cgsys.co.jp>
2024-11-01 19:15:40 +0900

    AHC012追加

---
+++ AHC012/AHC012.cpp
@@ -0,0 +1,242 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define drep(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define dsrep(i, s, t) for (int i = (t) - 1; i >= s; --i)
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> P;
+typedef pair<P, P> PP;
+
+// 乱数
+static uint32_t randxor()
+{
+  static uint32_t x = 123456789;
+  static uint32_t y = 362436069;
+  static uint32_t z = 521288629;
+  static uint32_t w = 88675123;
+  uint32_t t;
+
+  t = x ^ (x << 11);
+  x = y;
+  y = z;
+  z = w;
+  return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+}
+
+// 0以上1未満の小数をとる乱数
+static double rand01() { return (randxor() + 0.5) * (1.0 / UINT_MAX); }
+
+// 配列シャッフル
+void FisherYates(int* data, int n)
+{
+  for (int i = n - 1; i >= 0; i--) {
+    int j = randxor() % (i + 1);
+    int swa = data[i];
+    data[i] = data[j];
+    data[j] = swa;
+  }
+}
+
+// 配列シャッフル
+std::random_device seed_gen;
+std::mt19937 engine(seed_gen());
+// std::shuffle(v.begin(), v.end(), engine);
+
+
+const ll INF = 1001001001001001001;
+const int INT_INF = 1001001001;
+
+const int dx[4] = { -1, 0, 1, 0 };
+const int dy[4] = { 0, -1, 0, 1 };
+
+double TL = 1.8;
+int mode;
+std::chrono::steady_clock::time_point startTime, endTime;
+
+void ResetTime()
+{
+  startTime = std::chrono::steady_clock::now();
+}
+
+double GetNowTime()
+{
+  auto endTime = std::chrono::steady_clock::now();
+  std::chrono::duration<double> elapsed = endTime - startTime;
+  return elapsed.count();
+}
+
+
+const int MAX_N = 5500;
+
+int n;
+const int k = 100;
+int a[11];
+int x[MAX_N], y[MAX_N];
+
+int ansScore;
+
+int best_ansScore;
+
+void CopyToBest()
+{
+  best_ansScore = ansScore;
+}
+
+void CopyToAns()
+{
+  ansScore = best_ansScore;
+}
+
+// 複数ケース回すときに内部状態を初期値に戻す
+void SetUp()
+{
+  ansScore = 0;
+}
+
+// 入力受け取り
+void Input(int problemNum)
+{
+  std::ostringstream oss;
+  oss << "./in/" << std::setw(4) << std::setfill('0') << problemNum << ".txt";
+  ifstream ifs(oss.str());
+
+  if (!ifs.is_open()) {
+    // 標準入力
+  }
+  else {
+    // ファイル入力
+  }
+}
+
+// 出力ファイルストリームオープン
+void OpenOfs(int probNum, ofstream& ofs)
+{
+  if (mode != 0) {
+    std::ostringstream oss;
+    oss << "./out/" << std::setw(4) << std::setfill('0') << probNum << ".txt";
+    ofs.open(oss.str());
+  }
+}
+
+// スコア計算
+ll CalcScore()
+{
+  ll res = 0;
+  return res;
+}
+
+// 解答出力
+void Output(ofstream& ofs)
+{
+  if (mode == 0) {
+    // 標準出力
+  }
+  else {
+    // ファイル出力
+  }
+}
+
+// ナイーブな解法
+void Method1()
+{
+
+}
+
+ll Solve(int probNum)
+{
+  ResetTime();
+
+  // 複数ケース回すときに内部状態を初期値に戻す
+  SetUp();
+
+  // 入力受け取り
+  Input(probNum);
+
+  // 出力ファイルストリームオープン
+  ofstream ofs;
+  OpenOfs(probNum, ofs);
+
+  // 初期解生成
+  Method1();
+
+  // 解答を出力
+  Output(ofs);
+
+  if (ofs.is_open()) {
+    ofs.close();
+  }
+
+  ll score = 0;
+  if (mode != 0) {
+    score = CalcScore();
+  }
+  return score;
+}
+
+/////////////////////////////////////////////////////////////////////////
+/*
+メモ
+
+*/
+/////////////////////////////////////////////////////////////////////////
+int main()
+{
+  srand((unsigned)time(NULL));
+  while (rand() % 100) {
+    randxor();
+  }
+
+  mode = 2;
+
+  if (mode == 0) {
+    Solve(0);
+  }
+  else {
+    ll sum = 0;
+    srep(i, 0, 100)
+    {
+      ll score = Solve(i);
+      sum += score;
+      if (mode == 1) {
+        cout << score << endl;
+      }
+      else {
+        cout << "num = " << setw(2) << i << ", ";
+        cout << "score = " << setw(4) << score << ", ";
+        cout << "sum = " << setw(5) << sum << ", ";
+        cout << endl;
+      }
+    }
+  }
+
+  return 0;
+}
