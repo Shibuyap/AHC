99709bf77dadf84ab7514e0238625aecd68390f9
f-shibuya <f-shibuya@cgsys.co.jp>
2025-04-22 13:16:25 +0900

    ahc035

--- AHC035/AHC035.cpp
+++ AHC035/AHC035.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <array>
 #include <bitset>
 #include <cassert>
 #include <cctype>
@@ -27,51 +28,82 @@
 #include <string>
 #include <utility>
 #include <vector>
+
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 #define srep(i, s, t) for (int i = s; i < t; ++i)
-#define drep(i, n) for (int i = (n) - 1; i >= 0; --i)
-using namespace std;
-typedef long long int ll;
-typedef pair<ll, ll> P;
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define dsrep(i, s, t) for (int i = (t)-1; i >= s; --i)
 
-namespace /* 乱数ライブラリ */
+using namespace std;
+
+typedef long long int ll;
+typedef pair<int, int> P;
+typedef pair<P, P> PP;
+
+// タイマー
+namespace
 {
-  static uint32_t Rand()
+  std::chrono::steady_clock::time_point start_time_clock;
+
+  void start_timer()
+  {
+    start_time_clock = std::chrono::steady_clock::now();
+  }
+
+  double get_elapsed_time()
+  {
+    std::chrono::duration<double> elapsed = std::chrono::steady_clock::now() - start_time_clock;
+    return elapsed.count();
+  }
+}
+
+// 乱数
+namespace
+{
+  static uint32_t rand_xorshift()
   {
     static uint32_t x = 123456789;
     static uint32_t y = 362436069;
     static uint32_t z = 521288629;
     static uint32_t w = 88675123;
-    uint32_t t;
-
-    t = x ^ (x << 11);
+    uint32_t t = x ^ (x << 11);
     x = y;
     y = z;
     z = w;
-    return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+    w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+    return w;
   }
 
-
-  static double Rand01() {
-    return (Rand() + 0.5) * (1.0 / UINT_MAX);
+  static double rand_01()
+  {
+    return (rand_xorshift() + 0.5) * (1.0 / UINT_MAX);
   }
 
-  // 配列シャッフル
-  void FisherYates(int* data, int n)
+  static double rand_range(double l, double r)
+  {
+    return l + (r - l) * rand_01();
+  }
+
+  static uint32_t rand_range(uint32_t l, uint32_t r)
+  {
+    return l + rand_xorshift() % (r - l + 1); // [l, r]
+  }
+
+  void shuffle_array(int* arr, int n)
   {
     for (int i = n - 1; i >= 0; i--) {
-      int j = Rand() % (i + 1);
-      int swa = data[i];
-      data[i] = data[j];
-      data[j] = swa;
+      int j = rand_xorshift() % (i + 1);
+      int swa = arr[i];
+      arr[i] = arr[j];
+      arr[j] = swa;
     }
   }
-}  // namespace
+}
 
-const int dx[4] = { -1, 0, 1, 0 };
-const int dy[4] = { 0, -1, 0, 1 };
+const int DX[4] = { -1, 0, 1, 0 };
+const int DY[4] = { 0, -1, 0, 1 };
 
-bool IsNG(int x, int y, int n)
+bool is_out_of_bounds(int x, int y, int n)
 {
   if (x < 0 || n <= x || y < 0 || n <= y) return true;
   return false;
@@ -80,35 +112,28 @@ bool IsNG(int x, int y, int n)
 const ll INF = 1001001001001001001;
 const int INT_INF = 1001001001;
 
-double TL = 1.8;
-int mode;
-clock_t startTime, endTime;
+double TIME_LIMIT = 1.8;
+int exec_mode;
 
-const int N = 6;
-const int M = 15;
-const int T = 10;
-const int NN = 2 * 6 * (6 - 1);
-vector<vector<int>> X(NN, vector<int>(15, 0));
-int maxX[20];
-int XX[100][20];
-int order[1000];
+const int BOARD_SIZE = 6;
+const int ITEM_KIND = 15;
+const int TURN_COUNT = 10;
+const int EDGE_COUNT = 2 * BOARD_SIZE * (BOARD_SIZE - 1);
+vector<vector<int>> item_value(EDGE_COUNT, vector<int>(ITEM_KIND, 0));
+int item_value_max[ITEM_KIND];
+int item_value_pow3[100][ITEM_KIND];
+int sorted_order[EDGE_COUNT];
 
-vector<vector<int>> A(6, vector<int>(6, 0));
+vector<vector<int>> placement(BOARD_SIZE, vector<int>(BOARD_SIZE, 0));
 
-int uuu[10][6][6][15], vvv[10][6][6][15];
-
-double GetNowTime()
-{
-  endTime = clock();
-  double nowTime = ((double)endTime - startTime) / CLOCKS_PER_SEC;
-  return nowTime;
-}
+int horizontal_edge_block[TURN_COUNT][BOARD_SIZE][BOARD_SIZE][ITEM_KIND];
+int vertical_edge_block[TURN_COUNT][BOARD_SIZE][BOARD_SIZE][ITEM_KIND];
 
 // 複数ケース回すときに内部状態を初期値に戻す
-void SetUp() {}
+void reset_state() {}
 
 // 入力受け取り
-void Input(int problemNum)
+void read_input(int problemNum)
 {
   string fileNameIfs = "./in/";
   string strNum;
@@ -126,9 +151,9 @@ void Input(int problemNum)
   if (!ifs.is_open()) {
     int n, m, t;
     cin >> n >> m >> t;
-    for (int i = 0; i < NN; i++) {
-      for (int j = 0; j < M; j++) {
-        cin >> X[i][j];
+    for (int i = 0; i < EDGE_COUNT; i++) {
+      for (int j = 0; j < ITEM_KIND; j++) {
+        cin >> item_value[i][j];
       }
     }
   }
@@ -136,53 +161,53 @@ void Input(int problemNum)
   else {
     int n, m, t;
     ifs >> n >> m >> t;
-    for (int i = 0; i < NN; i++) {
-      for (int j = 0; j < M; j++) {
-        ifs >> X[i][j];
+    for (int i = 0; i < EDGE_COUNT; i++) {
+      for (int j = 0; j < ITEM_KIND; j++) {
+        ifs >> item_value[i][j];
       }
     }
-    rep(i, T)
+    rep(i, TURN_COUNT)
     {
-      rep(j, N)
+      rep(j, BOARD_SIZE)
       {
-        rep(k, N - 1)
+        rep(k, BOARD_SIZE - 1)
         {
-          string sss;
-          ifs >> sss;
-          rep(l, M) {
-            uuu[i][j][k][l] = sss[l] - '0';
+          string s;
+          ifs >> s;
+          rep(l, ITEM_KIND) {
+            horizontal_edge_block[i][j][k][l] = s[l] - '0';
           }
         }
       }
-      rep(j, N - 1)
+      rep(j, BOARD_SIZE - 1)
       {
-        rep(k, N)
+        rep(k, BOARD_SIZE)
         {
-          string sss;
-          ifs >> sss;
-          rep(l, M) {
-            vvv[i][j][k][l] = sss[l] - '0';
+          string s;
+          ifs >> s;
+          rep(l, ITEM_KIND) {
+            vertical_edge_block[i][j][k][l] = s[l] - '0';
           }
         }
       }
     }
   }
 
-  rep(j, M) {
-    maxX[j] = 0;
+  rep(j, ITEM_KIND) {
+    item_value_max[j] = 0;
   }
-  rep(i, NN)
+  rep(i, EDGE_COUNT)
   {
-    rep(j, M) {
-      maxX[j] = max(maxX[j], X[i][j]);
+    rep(j, ITEM_KIND) {
+      item_value_max[j] = max(item_value_max[j], item_value[i][j]);
     }
   }
 }
 
 // 出力ファイルストリームオープン
-void OpenOfs(int probNum, ofstream& ofs)
+void open_output_file(int probNum, ofstream& ofs)
 {
-  if (mode != 0) {
+  if (exec_mode != 0) {
     string fileNameOfs = "./out/";
     string strNum;
     rep(i, 4)
@@ -198,19 +223,19 @@ void OpenOfs(int probNum, ofstream& ofs)
 }
 
 // スコア計算
-ll CalcScore()
+ll calc_final_score()
 {
   ll sumX = 0;
-  rep(j, M) {
-    sumX += maxX[j];
+  rep(j, ITEM_KIND) {
+    sumX += item_value_max[j];
   }
 
   int ma = 0;
-  rep(i, NN)
+  rep(i, EDGE_COUNT)
   {
     int tmp = 0;
-    rep(j, M) {
-      tmp += X[i][j];
+    rep(j, ITEM_KIND) {
+      tmp += item_value[i][j];
     }
     ma = max(ma, tmp);
   }
@@ -219,97 +244,96 @@ ll CalcScore()
   return res;
 }
 
-void GetReturn(int turn)
+void apply_feedback(int turn)
 {
-  int now = 0;
-  rep(i, N)
-  {
-    rep(j, N - 1)
-    {
-      rep(k, M)
-      {
-        if (uuu[turn % T][i][j][k] == 0) {
-          X[now][k] = XX[A[i][j]][k];
+  // placementに基づいて itemValue を再設定
+  int idx = 0;
+  // horizontal
+  for (int row = 0; row < BOARD_SIZE; row++) {
+    for (int col = 0; col < BOARD_SIZE - 1; col++) {
+      for (int k = 0; k < ITEM_KIND; k++) {
+        if (horizontal_edge_block[turn][row][col][k] == 0) {
+          item_value[idx][k] = item_value_pow3[placement[row][col]][k];
         }
         else {
-          X[now][k] = XX[A[i][j + 1]][k];
+          item_value[idx][k] = item_value_pow3[placement[row][col + 1]][k];
         }
       }
-      now++;
+      idx++;
     }
   }
-  rep(i, N - 1)
-  {
-    rep(j, N)
-    {
-      rep(k, M)
-      {
-        if (vvv[turn % T][i][j][k] == 0) {
-          X[now][k] = XX[A[i][j]][k];
+  // vertical
+  for (int row = 0; row < BOARD_SIZE - 1; row++) {
+    for (int col = 0; col < BOARD_SIZE; col++) {
+      for (int k = 0; k < ITEM_KIND; k++) {
+        if (vertical_edge_block[turn][row][col][k] == 0) {
+          item_value[idx][k] = item_value_pow3[placement[row][col]][k];
         }
         else {
-          X[now][k] = XX[A[i + 1][j]][k];
+          item_value[idx][k] = item_value_pow3[placement[row + 1][col]][k];
         }
       }
-      now++;
+      idx++;
     }
   }
 }
 
-void Convert()
+void precompute_values()
 {
-  vector<P> v[M];
-  rep(i, NN)
+  vector<P> v[ITEM_KIND];
+  rep(i, EDGE_COUNT)
   {
-    rep(j, M) {
-      v[j].push_back(P(X[i][j], i));
+    rep(j, ITEM_KIND) {
+      v[j].push_back(P(item_value[i][j], i));
     }
   }
-  rep(i, M) {
+  rep(i, ITEM_KIND) {
     sort(v[i].begin(), v[i].end(), greater<P>());
   }
-  int val[NN][M];
-  rep(i, NN)
+  int val[EDGE_COUNT][ITEM_KIND];
+  rep(i, EDGE_COUNT)
   {
-    rep(j, M) {
+    rep(j, ITEM_KIND) {
       val[v[j][i].second][j] = i;
     }
   }
 
-  int ma[M] = {};
-  rep(i, NN)
+  int ma[ITEM_KIND] = {};
+  rep(i, EDGE_COUNT)
   {
-    rep(j, M) {
-      ma[j] = max(ma[j], X[i][j]);
+    rep(j, ITEM_KIND) {
+      ma[j] = max(ma[j], item_value[i][j]);
     }
   }
 
-  rep(i, NN)
+  rep(i, EDGE_COUNT)
   {
-    rep(j, M)
+    rep(j, ITEM_KIND)
     {
-      XX[i][j] = X[i][j];
-      X[i][j] = X[i][j] * X[i][j] * X[i][j];
+      item_value_pow3[i][j] = item_value[i][j];
+      item_value[i][j] = item_value[i][j] * item_value[i][j] * item_value[i][j];
     }
   }
 }
 
-void Sort()
+void sort_by_total_value()
 {
   vector<P> vp;
-  rep(i, NN)
+  rep(i, EDGE_COUNT)
   {
     int sum = 0;
-    rep(j, M) sum += X[i][j];
+    rep(j, ITEM_KIND) {
+      sum += item_value[i][j];
+    }
     vp.push_back(P(sum, i));
   }
   sort(vp.begin(), vp.end(), greater<P>());
   rep(i, vp.size()) {
-    order[i] = vp[i].second;
+    sorted_order[i] = vp[i].second;
   }
 }
 
-const int o[6][6] = {
+const int SPIRAL_RANK[6][6] = {
     {21, 20, 19, 18, 17, 16},
     {22,  7,  6,  5, 16, 35},
     {23,  8,  1,  4, 15, 34},
@@ -318,110 +342,110 @@ const int o[6][6] = {
     {26, 27, 28, 29, 30, 31},
 };
 
-vector<P> guruguruOrder;
-void InitGuruGuru()
+vector<P> spiral_cells;
+void init_spiral_order()
 {
-  guruguruOrder.clear();
+  spiral_cells.clear();
   vector<pair<int, P>> vp;
   rep(i, 6)
   {
     rep(j, 6) {
-      vp.push_back(make_pair(o[i][j], P(i, j)));
+      vp.push_back(make_pair(SPIRAL_RANK[i][j], P(i, j)));
     }
   }
   sort(vp.begin(), vp.end());
   for (auto p : vp) {
-    guruguruOrder.push_back(p.second);
+    spiral_cells.push_back(p.second);
   }
 }
 
-ll InnerGuruGuru(int x, int y)
+ll local_neighbor_score(int x, int y)
 {
   ll res = 0;
-  int id = A[x][y];
+  int id = placement[x][y];
   rep(i, 4)
   {
-    int nx = x + dx[i];
-    int ny = y + dy[i];
-    if (IsNG(nx, ny, N)) {
+    int nx = x + DX[i];
+    int ny = y + DY[i];
+    if (is_out_of_bounds(nx, ny, BOARD_SIZE)) {
       continue;
     }
-    int nd = A[nx][ny];
-    rep(j, M) {
-      res += 10 * X[id][j] + 2 * abs(X[id][j] - X[nd][j]);
+    int nd = placement[nx][ny];
+    rep(j, ITEM_KIND) {
+      res += 10 * item_value[id][j] + 2 * abs(item_value[id][j] - item_value[nd][j]);
     }
   }
   return res;
 }
 
-void GuruGuru()
+void anneal_spiral_placement()
 {
   rep(i, 36)
   {
-    int x = guruguruOrder[i].first;
-    int y = guruguruOrder[i].second;
-    A[x][y] = order[i];
+    int x = spiral_cells[i].first;
+    int y = spiral_cells[i].second;
+    placement[x][y] = sorted_order[i];
   }
 
-  const int LOOP_COUNT = 250000;
+  const int LOOP_COUNT = 500000;
   rep(_, LOOP_COUNT)
   {
-    int ra1 = Rand() % 6;
-    int ra2 = Rand() % 6;
-    int ra3 = Rand() % 6;
-    int ra4 = Rand() % 6;
+    int ra1 = rand_xorshift() % 6;
+    int ra2 = rand_xorshift() % 6;
+    int ra3 = rand_xorshift() % 6;
+    int ra4 = rand_xorshift() % 6;
 
     int before = 0;
-    before += InnerGuruGuru(ra1, ra2);
-    before += InnerGuruGuru(ra3, ra4);
+    before += local_neighbor_score(ra1, ra2);
+    before += local_neighbor_score(ra3, ra4);
 
-    swap(A[ra1][ra2], A[ra3][ra4]);
+    swap(placement[ra1][ra2], placement[ra3][ra4]);
 
     int after = 0;
-    after += InnerGuruGuru(ra1, ra2);
-    after += InnerGuruGuru(ra3, ra4);
+    after += local_neighbor_score(ra1, ra2);
+    after += local_neighbor_score(ra3, ra4);
 
-    double temp = (double)(LOOP_COUNT - _) / LOOP_COUNT * 10;
+    double temp = (double)(LOOP_COUNT - _) / LOOP_COUNT * 500000;
     const double prob = exp((double)(after - before) / temp);
 
-    if (prob > Rand01()) {
+    if (prob > rand_01()) {
     }
     else {
-      swap(A[ra1][ra2], A[ra3][ra4]);
+      swap(placement[ra1][ra2], placement[ra3][ra4]);
     }
   }
 }
 
-ll Solve(int probNum)
+ll solve_case(int probNum)
 {
   // 複数ケース回すときに内部状態を初期値に戻す
-  SetUp();
+  reset_state();
 
-  InitGuruGuru();
+  init_spiral_order();
 
   // 入力受け取り
-  Input(probNum);
+  read_input(probNum);
 
   // 出力ファイルストリームオープン
   ofstream ofs;
-  OpenOfs(probNum, ofs);
+  open_output_file(probNum, ofs);
 
-  for (int t = 0; t < T; t++) {
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        A[i][j] = i * N + j;
+  for (int t = 0; t < TURN_COUNT; t++) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+      for (int j = 0; j < BOARD_SIZE; j++) {
+        placement[i][j] = i * BOARD_SIZE + j;
       }
     }
 
-    Convert();
-    Sort();
-    GuruGuru();
+    precompute_values();
+    sort_by_total_value();
+    anneal_spiral_placement();
 
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        if (mode == 0) {
-          cout << A[i][j];
-          if (j < N - 1) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+      for (int j = 0; j < BOARD_SIZE; j++) {
+        if (exec_mode == 0) {
+          cout << placement[i][j];
+          if (j < BOARD_SIZE - 1) {
             cout << " ";
           }
           else {
@@ -429,8 +453,8 @@ ll Solve(int probNum)
           }
         }
         else {
-          ofs << A[i][j];
-          if (j < N - 1) {
+          ofs << placement[i][j];
+          if (j < BOARD_SIZE - 1) {
             ofs << " ";
           }
           else {
@@ -440,16 +464,16 @@ ll Solve(int probNum)
       }
     }
 
-    if (mode == 0) {
+    if (exec_mode == 0) {
       cout.flush();
-      for (int i = 0; i < NN; i++) {
-        for (int j = 0; j < M; j++) {
-          cin >> X[i][j];
+      for (int i = 0; i < EDGE_COUNT; i++) {
+        for (int j = 0; j < ITEM_KIND; j++) {
+          cin >> item_value[i][j];
         }
       }
     }
     else {
-      GetReturn(t);
+      apply_feedback(t);
     }
   }
 
@@ -458,8 +482,8 @@ ll Solve(int probNum)
   }
 
   ll score = 0;
-  if (mode != 0) {
-    score = CalcScore();
+  if (exec_mode != 0) {
+    score = calc_final_score();
   }
 
   return score;
@@ -467,21 +491,16 @@ ll Solve(int probNum)
 
 int main()
 {
-  srand((unsigned)time(NULL));
-  while (rand() % 100) {
-    Rand();
-  }
+  exec_mode = 1;
 
-  mode = 1;
-
-  if (mode == 0) {
-    Solve(0);
+  if (exec_mode == 0) {
+    solve_case(0);
   }
-  else if (mode == 1) {
+  else if (exec_mode == 1) {
     ll sum = 0;
     srep(i, 0, 100)
     {
-      ll score = Solve(i);
+      ll score = solve_case(i);
       sum += score;
       cout << "num = " << i << ", ";
       cout << "score = " << score << ", ";
