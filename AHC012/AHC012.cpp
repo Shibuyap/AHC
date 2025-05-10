5db4c0699fb4f600d104b3feefff3cd0894abf21
Shibuyap <fumitaka.shibuya@gmail.com>
2025-05-10 17:00:20 +0900

    ahc012

--- AHC012/AHC012.cpp
+++ AHC012/AHC012.cpp
@@ -122,22 +122,170 @@ public:
   static constexpr int MIN = 0;
   static constexpr int MAX = 20000;
 
-  vector<int> xs, ys;
+  vector<int> xs, ys; // 半開区間 : [xs[i], xs[i+1])
+
+  vector<vector<int>> counts;
+  int b[11];
 
   void initialize(int v_num, int h_num) {
     xs.clear();
     xs.push_back(MIN);
-    for (int i = 1; i <= v_num + 1; i++) {
+    for (int i = 1; i < v_num + 1; i++) {
       xs.push_back(MIN + (MAX - MIN) * i / (v_num + 1));
     }
     xs.push_back(MAX);
 
     ys.clear();
     ys.push_back(MIN);
-    for (int i = 1; i <= h_num + 1; i++) {
+    for (int i = 1; i < h_num + 1; i++) {
       ys.push_back(MIN + (MAX - MIN) * i / (h_num + 1));
     }
     ys.push_back(MAX);
+  }
+
+  void calc_counts(const Board& board) {
+    for (int i = 0; i < 11; i++) {
+      b[i] = 0;
+    }
+
+    counts.resize(xs.size() - 1, vector<int>(ys.size() - 1));
+    for (int i = 0; i < xs.size() - 1; i++) {
+      int sx = xs[i];
+      int gx = xs[i + 1];
+      for (int j = 0; j < ys.size() - 1; j++) {
+        int sy = ys[j];
+        int gy = ys[j + 1];
+
+        counts[i][j] = 0;
+        for (int k = sx; k < gx; k++) {
+          for (auto l : board.x[k]) {
+            if (sy <= l && l < gy) {
+              counts[i][j]++;
+            }
+          }
+        }
+
+        if (1 <= counts[i][j] && counts[i][j] <= 10) {
+          b[counts[i][j]]++;
+        }
+      }
+    }
+  }
+
+  void update_xs(const Board& board, int num, int diff) {
+    if (diff < 0) {
+      for (int i = xs[num] + diff; i < xs[num]; ++i) {
+        for (auto y : board.x[i]) {
+          for (int j = 0; j < ys.size() - 1; j++) {
+            if (ys[j] <= y && y < ys[j + 1]) {
+              // 上側を減らす
+              if (1 <= counts[num - 1][j] && counts[num - 1][j] <= 10) {
+                b[counts[num - 1][j]]--;
+              }
+              counts[num - 1][j]--;
+              if (1 <= counts[num - 1][j] && counts[num - 1][j] <= 10) {
+                b[counts[num - 1][j]]++;
+              }
+
+              // 下側を増やす
+              if (1 <= counts[num][j] && counts[num][j] <= 10) {
+                b[counts[num][j]]--;
+              }
+              counts[num][j]++;
+              if (1 <= counts[num][j] && counts[num][j] <= 10) {
+                b[counts[num][j]]++;
+              }
+            }
+          }
+        }
+      }
+    }
+    else {
+      for (int i = xs[num]; i <= xs[num] + diff - 1; ++i) {
+        for (auto y : board.x[i]) {
+          for (int j = 0; j < ys.size() - 1; j++) {
+            if (ys[j] <= y && y < ys[j + 1]) {
+              // 上側を増やす
+              if (1 <= counts[num - 1][j] && counts[num - 1][j] <= 10) {
+                b[counts[num - 1][j]]--;
+              }
+              counts[num - 1][j]++;
+              if (1 <= counts[num - 1][j] && counts[num - 1][j] <= 10) {
+                b[counts[num - 1][j]]++;
+              }
+
+              // 下側を減らす
+              if (1 <= counts[num][j] && counts[num][j] <= 10) {
+                b[counts[num][j]]--;
+              }
+              counts[num][j]--;
+              if (1 <= counts[num][j] && counts[num][j] <= 10) {
+                b[counts[num][j]]++;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    xs[num] += diff;
+  }
+
+  void update_ys(const Board& board, int num, int diff) {
+    if (diff < 0) {
+      for (int i = ys[num] + diff; i < ys[num]; ++i) {
+        for (auto x : board.y[i]) {
+          for (int j = 0; j < xs.size() - 1; j++) {
+            if (xs[j] <= x && x < xs[j + 1]) {
+              // 左側を減らす
+              if (1 <= counts[j][num - 1] && counts[j][num - 1] <= 10) {
+                b[counts[j][num - 1]]--;
+              }
+              counts[j][num - 1]--;
+              if (1 <= counts[j][num - 1] && counts[j][num - 1] <= 10) {
+                b[counts[j][num - 1]]++;
+              }
+              // 右側を増やす
+              if (1 <= counts[j][num] && counts[j][num] <= 10) {
+                b[counts[j][num]]--;
+              }
+              counts[j][num]++;
+              if (1 <= counts[j][num] && counts[j][num] <= 10) {
+                b[counts[j][num]]++;
+              }
+            }
+          }
+        }
+      }
+    }
+    else {
+      for (int i = ys[num]; i <= ys[num] + diff - 1; ++i) {
+        for (auto x : board.y[i]) {
+          for (int j = 0; j < xs.size() - 1; j++) {
+            if (xs[j] <= x && x < xs[j + 1]) {
+              // 左側を増やす
+              if (1 <= counts[j][num - 1] && counts[j][num - 1] <= 10) {
+                b[counts[j][num - 1]]--;
+              }
+              counts[j][num - 1]++;
+              if (1 <= counts[j][num - 1] && counts[j][num - 1] <= 10) {
+                b[counts[j][num - 1]]++;
+              }
+              // 右側を減らす
+              if (1 <= counts[j][num] && counts[j][num] <= 10) {
+                b[counts[j][num]]--;
+              }
+              counts[j][num]--;
+              if (1 <= counts[j][num] && counts[j][num] <= 10) {
+                b[counts[j][num]]++;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    ys[num] += diff;
   }
 };
 
@@ -187,33 +335,9 @@ Board input_data(int case_num) {
 }
 
 int calculate_score(const Board& board, const Answer& answer) {
-  int b[11] = {};
-  for (int i = 0; i < answer.xs.size() - 1; i++) {
-    int sx = answer.xs[i];
-    int gx = answer.xs[i + 1];
-    for (int j = 0; j < answer.ys.size() - 1; j++) {
-      int sy = answer.ys[j];
-      int gy = answer.ys[j + 1];
-      int cnt = 0;
-      for (int k = sx; k < gx; k++) {
-        for (auto l : board.x[k]) {
-          if (sy <= l && l < gy) {
-            cnt++;
-          }
-        }
-        if (cnt > 10) {
-          break;
-        }
-      }
-      if (1 <= cnt && cnt <= 10) {
-        b[cnt]++;
-      }
-    }
-  }
-
   int ok_cnt = 0;
   for (int i = 1; i <= 10; i++) {
-    ok_cnt += min(b[i], board.a[i]);
+    ok_cnt += min(answer.b[i], board.a[i]);
   }
 
   int res = round(1e6 * ok_cnt / board.a_sum);
@@ -258,11 +382,12 @@ Answer build_initial_solution(const Board& board) {
   int iter = 0;
   while (get_elapsed_time() < TIME_LIMIT / 3) {
     iter++;
-    int sum = rand_xorshift() % 70 + 30;
+    int sum = rand_xorshift() % 71 + 30;
     int v_num = rand_xorshift() % (sum - 1) + 1;
     int h_num = sum - v_num;
     Answer answer;
     answer.initialize(v_num, h_num);
+    answer.calc_counts(board);
     int score = calculate_score(board, answer);
     if (score > best_score) {
       best_score = score;
@@ -274,13 +399,30 @@ Answer build_initial_solution(const Board& board) {
   return best_answer;
 }
 
-void annealing(const Board& board, Answer& answer) {
+struct AnnealingParams
+{
+  double start_temperature;
+  double end_temperature;
+  double score_scale;
+};
+
+void annealing(const Board& board, Answer& answer, const AnnealingParams& params) {
   Answer best_answer = answer;
+  answer.calc_counts(board);
   int best_score = calculate_score(board, answer);
 
+  double start_time = get_elapsed_time();
+  double now_time = start_time;
   int iter = 0;
-  while (get_elapsed_time() < TIME_LIMIT) {
+  while (true) {
     iter++;
+
+    if (iter % 100 == 0) {
+      now_time = get_elapsed_time();
+      if (now_time > TIME_LIMIT) {
+        break;
+      }
+    }
 
     int choice = rand_xorshift() % 2;
     int num = 1;
@@ -291,7 +433,11 @@ void annealing(const Board& board, Answer& answer) {
       num = rand_xorshift() % (answer.ys.size() - 2) + 1;
     }
     int diff = rand_xorshift() % 61 - 30;
+    while (diff == 0) {
+      diff = rand_xorshift() % 61 - 30;
+    }
 
+    int current_score = calculate_score(board, answer);
     if (choice == 0) {
       if (answer.xs[num] + diff <= answer.xs[num - 1]) {
         continue;
@@ -299,7 +445,7 @@ void annealing(const Board& board, Answer& answer) {
       if (answer.xs[num] + diff >= answer.xs[num + 1]) {
         continue;
       }
-      answer.xs[num] += diff;
+      answer.update_xs(board, num, diff);
     }
     else {
       if (answer.ys[num] + diff <= answer.ys[num - 1]) {
@@ -308,20 +454,29 @@ void annealing(const Board& board, Answer& answer) {
       if (answer.ys[num] + diff >= answer.ys[num + 1]) {
         continue;
       }
-      answer.ys[num] += diff;
+      answer.update_ys(board, num, diff);
     }
 
+    double progress_ratio = (now_time - start_time) / (TIME_LIMIT - start_time);
+    double temp = params.start_temperature + (params.end_temperature - params.start_temperature) * progress_ratio;
+
     int new_score = calculate_score(board, answer);
-    if (new_score >= best_score) {
-      best_score = new_score;
-      best_answer = answer;
+    double diff_score = (new_score - current_score) * params.score_scale;
+    double prob = exp(diff_score / temp);
+    if (prob > rand_01()) {
+      // 採用
+
+      if (new_score > best_score) {
+        best_score = new_score;
+        best_answer = answer;
+      }
     }
     else {
       if (choice == 0) {
-        answer.xs[num] -= diff;
+        answer.update_xs(board, num, -diff);
       }
       else {
-        answer.ys[num] -= diff;
+        answer.update_ys(board, num, -diff);
       }
     }
   }
@@ -338,13 +493,18 @@ ll solve_case(int case_num) {
 
   Answer answer = build_initial_solution(board);
 
-  annealing(board, answer);
+  AnnealingParams params;
+  params.start_temperature = 10000048.0;
+  params.end_temperature = 0.0;
+  params.score_scale = 12345.0;
+  annealing(board, answer, params);
 
   output_data(case_num, answer);
 
   int score = 0;
   if (exec_mode != 0) {
     score = calculate_score(board, answer);
+    cerr << answer.xs.size() + answer.ys.size() - 4 << endl;
   }
   return score;
 }
