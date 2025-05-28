ae3e29abd17028569cd72da203df922a5feb2ddd
f-shibuya <f-shibuya@cgsys.co.jp>
2025-05-28 14:18:13 +0900

    ahc032

--- AHC032/AHC032.cpp
+++ AHC032/AHC032.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <array>
 #include <bitset>
 #include <cassert>
 #include <cctype>
@@ -65,88 +66,66 @@ const int n = 9;
 const int m = 20;
 const int T = 81;
 const int MOD = 998244353;
-int board[n][n];
+
 int initial_board[n][n];
 int magic_pattern[m][3][3];
-int current_solution[T][3];
-ll current_score;
 
-int best_board[n][n];
-ll best_solution[T][3];
-ll best_score;
-
-void CopyToBest()
+class State
 {
-  best_score = current_score;
-  for (int i = 0; i < T; ++i) {
-    for (int j = 0; j < 3; ++j) {
-      best_solution[i][j] = current_solution[i][j];
+public:
+  array<array<int, n>, n> board;
+  array<array<int, 3>, T> solution; // solution[i][0]: magic index, solution[i][1]: x, solution[i][2]: y
+  ll score;
+  State()
+  {
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        board[i][j] = 0;
+      }
     }
-  }
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      best_board[i][j] = board[i][j];
+    for (int i = 0; i < T; ++i) {
+      solution[i][0] = -1;
+      solution[i][1] = -1;
+      solution[i][2] = -1;
     }
+    score = 0;
   }
-}
-
-void CopyFromBest()
-{
-  current_score = best_score;
-  for (int i = 0; i < T; ++i) {
-    for (int j = 0; j < 3; ++j) {
-      current_solution[i][j] = best_solution[i][j];
-    }
-  }
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      board[i][j] = best_board[i][j];
-    }
-  }
-}
+};
 
 // スコア計算
-ll CalcScore()
+ll CalcScore(const State& current)
 {
   ll res = 0;
 
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
-      res += board[i][j] % MOD;
+      res += current.board[i][j] % MOD;
     }
   }
 
   return res;
 }
 
-// 複数ケース回すときに内部状態を初期値に戻す
-void SetUp()
+void InitializeAns(State& current)
 {
   for (int i = 0; i < T; ++i) {
     for (int j = 0; j < 3; ++j) {
-      current_solution[i][j] = -1;
-    }
-  }
-}
-
-void InitializeAns()
-{
-  for (int i = 0; i < T; ++i) {
-    for (int j = 0; j < 3; ++j) {
-      current_solution[i][j] = -1;
+      current.solution[i][j] = -1;
     }
   }
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
-      board[i][j] = initial_board[i][j];
+      current.board[i][j] = initial_board[i][j];
     }
   }
-  current_score = CalcScore();
+  current.score = CalcScore(current);
 }
 
 // 入力受け取り
-void Input(int problemNum)
+State Input(int problemNum)
 {
+  State current;
+
   string fileNameIfs = "./in/";
   string strNum;
   for (int i = 0; i < 4; ++i) {
@@ -164,7 +143,7 @@ void Input(int problemNum)
     cin >> _n >> _m >> _k;
     for (int i = 0; i < n; ++i) {
       for (int j = 0; j < n; ++j) {
-        cin >> board[i][j];
+        cin >> current.board[i][j];
       }
     }
     for (int i = 0; i < m; ++i) {
@@ -181,7 +160,7 @@ void Input(int problemNum)
     ifs >> _n >> _m >> _k;
     for (int i = 0; i < n; ++i) {
       for (int j = 0; j < n; ++j) {
-        ifs >> board[i][j];
+        ifs >> current.board[i][j];
       }
     }
     for (int i = 0; i < m; ++i) {
@@ -195,9 +174,11 @@ void Input(int problemNum)
 
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
-      initial_board[i][j] = board[i][j];
+      initial_board[i][j] = current.board[i][j];
     }
   }
+
+  return current;
 }
 
 // 出力ファイルストリームオープン
@@ -218,38 +199,45 @@ void OpenOfs(int probNum, ofstream& ofs)
 }
 
 // 初期解生成
-void Initialize()
+void Initialize(State& current)
 {
   for (int i = 0; i < T; ++i) {
-    current_solution[i][0] = -1;
+    current.solution[i][0] = -1;
   }
-  current_score = CalcScore();
-  CopyToBest();
+  current.score = CalcScore(current);
 }
 
 // 解答出力
-void Output(ofstream& ofs)
+void Output(int probNum, const State& current)
 {
+  ofstream ofs;
+  OpenOfs(probNum, ofs);
+
   int L = 0;
   for (int i = 0; i < T; ++i) {
-    if (current_solution[i][0] == -1) continue;
+    if (current.solution[i][0] == -1) continue;
     L++;
   }
+
   if (mode == 0) {
     cout << L << endl;
     for (int i = 0; i < T; ++i) {
-      if (current_solution[i][0] == -1) continue;
-      for (int j = 0; j < 3; ++j) cout << current_solution[i][j] << ' ';
+      if (current.solution[i][0] == -1) continue;
+      for (int j = 0; j < 3; ++j) cout << current.solution[i][j] << ' ';
       cout << endl;
     }
   }
   else {
     ofs << L << endl;
     for (int i = 0; i < T; ++i) {
-      if (current_solution[i][0] == -1) continue;
-      for (int j = 0; j < 3; ++j) ofs << current_solution[i][j] << ' ';
+      if (current.solution[i][0] == -1) continue;
+      for (int j = 0; j < 3; ++j) ofs << current.solution[i][j] << ' ';
       ofs << endl;
     }
+  }
+
+  if (ofs.is_open()) {
+    ofs.close();
   }
 }
 
@@ -416,8 +404,10 @@ int keepA[n][n];
 int keepAns[110][3];
 int baseA[n][n];
 
-void Method4(double timeLimit)
+void Method4(double timeLimit, State& current)
 {
+  State best = current;
+
   int loopCount = 0;
   while (true) {
     loopCount++;
@@ -427,7 +417,7 @@ void Method4(double timeLimit)
     ll hosyou = Rand() % 200000000 + MOD - 200000000;
 
     int ng = 0;
-    InitializeAns();
+    InitializeAns(current);
     int cnt = 0;
     int dir1 = Rand() % 2;
     int dir2 = Rand() % 2;
@@ -446,12 +436,11 @@ void Method4(double timeLimit)
       int maCntTail = 0;
       for (int p = 0; p < n; ++p) {
         for (int q = 0; q < n; ++q) {
-          baseA[p][q] = board[p][q];
+          baseA[p][q] = current.board[p][q];
         }
       }
 
       for (int jjj = 0; jjj < n - 2; ++jjj) {
-        int ng2 = 0;
         for (int jj = 0; jj < jjj; ++jj) {
           int j = jj;
           dir2 = 0;
@@ -460,7 +449,7 @@ void Method4(double timeLimit)
           maxSum = 0;
           for (int k = 0; k < 3; ++k) {
             for (int l = 0; l < 3; ++l) {
-              ma[k][l] = board[i + k][j + l];
+              ma[k][l] = current.board[i + k][j + l];
               now[k][l] = ma[k][l];
             }
           }
@@ -480,7 +469,7 @@ void Method4(double timeLimit)
             if (maxSum < hosyou && Rand() % 3 != 0) {
               for (int p = 0; p < 3; ++p) {
                 for (int q = 0; q < 3; ++q) {
-                  now[p][q] = board[i + p][j + q];
+                  now[p][q] = current.board[i + p][j + q];
                 }
               }
               Method2DFS(0, 0, 2);
@@ -499,9 +488,11 @@ void Method4(double timeLimit)
 
           for (int k = 0; k < maAnsCount; ++k) {
             int ansM = maAnssArr[k];
-            current_solution[cnt][0] = ansM;
-            current_solution[cnt][1] = i;
-            current_solution[cnt][2] = j;
+            if (cnt < T) {
+              current.solution[cnt][0] = ansM;
+              current.solution[cnt][1] = i;
+              current.solution[cnt][2] = j;
+            }
             cnt++;
           }
           if (cnt > T) {
@@ -510,7 +501,7 @@ void Method4(double timeLimit)
           }
           for (int k = 0; k < 3; ++k) {
             for (int l = 0; l < 3; ++l) {
-              board[i + k][j + l] = ma[k][l];
+              current.board[i + k][j + l] = ma[k][l];
             }
           }
         }
@@ -523,7 +514,7 @@ void Method4(double timeLimit)
           maxSum = 0;
           for (int k = 0; k < 3; ++k) {
             for (int l = 0; l < 3; ++l) {
-              ma[k][l] = board[i + k][j + l];
+              ma[k][l] = current.board[i + k][j + l];
               now[k][l] = ma[k][l];
             }
           }
@@ -543,7 +534,7 @@ void Method4(double timeLimit)
             if (maxSum < hosyou && Rand() % 3 != 0) {
               for (int p = 0; p < 3; ++p) {
                 for (int q = 0; q < 3; ++q) {
-                  now[p][q] = board[i + p][j + q];
+                  now[p][q] = current.board[i + p][j + q];
                 }
               }
               Method2DFS(0, 0, 2);
@@ -562,9 +553,11 @@ void Method4(double timeLimit)
 
           for (int k = 0; k < maAnsCount; ++k) {
             int ansM = maAnssArr[k];
-            current_solution[cnt][0] = ansM;
-            current_solution[cnt][1] = i;
-            current_solution[cnt][2] = j;
+            if (cnt < T) {
+              current.solution[cnt][0] = ansM;
+              current.solution[cnt][1] = i;
+              current.solution[cnt][2] = j;
+            }
             cnt++;
           }
           if (cnt > T) {
@@ -573,7 +566,7 @@ void Method4(double timeLimit)
           }
           for (int k = 0; k < 3; ++k) {
             for (int l = 0; l < 3; ++l) {
-              board[i + k][j + l] = ma[k][l];
+              current.board[i + k][j + l] = ma[k][l];
             }
           }
         }
@@ -584,7 +577,7 @@ void Method4(double timeLimit)
           maxSum = 0;
           for (int k = 0; k < 3; ++k) {
             for (int l = 0; l < 3; ++l) {
-              ma[k][l] = board[i + k][j + l];
+              ma[k][l] = current.board[i + k][j + l];
               now[k][l] = ma[k][l];
             }
           }
@@ -605,7 +598,7 @@ void Method4(double timeLimit)
             if (maxSum < hosyou && Rand() % 3 != 0) {
               for (int p = 0; p < 3; ++p) {
                 for (int q = 0; q < 3; ++q) {
-                  now[p][q] = board[i + p][j + q];
+                  now[p][q] = current.board[i + p][j + q];
                 }
               }
               Method2DFS(0, 0, 2);
@@ -625,9 +618,11 @@ void Method4(double timeLimit)
 
           for (int k = 0; k < maAnsCount; ++k) {
             int ansM = maAnssArr[k];
-            current_solution[cnt][0] = ansM;
-            current_solution[cnt][1] = i;
-            current_solution[cnt][2] = j;
+            if (cnt < T) {
+              current.solution[cnt][0] = ansM;
+              current.solution[cnt][1] = i;
+              current.solution[cnt][2] = j;
+            }
             cnt++;
           }
           if (cnt > T) {
@@ -636,7 +631,7 @@ void Method4(double timeLimit)
           }
           for (int k = 0; k < 3; ++k) {
             for (int l = 0; l < 3; ++l) {
-              board[i + k][j + l] = ma[k][l];
+              current.board[i + k][j + l] = ma[k][l];
             }
           }
         }
@@ -647,34 +642,34 @@ void Method4(double timeLimit)
 
         if (dir1 == 0) {
           for (int j = 0; j < n; ++j) {
-            tmpPosSum += board[i][j];
+            tmpPosSum += current.board[i][j];
           }
           if (i == n - 3) {
             for (int j = 0; j < n; ++j) {
-              tmpPosSum += board[i + 1][j];
-              tmpPosSum += board[i + 2][j];
+              tmpPosSum += current.board[i + 1][j];
+              tmpPosSum += current.board[i + 2][j];
             }
           }
         }
         else {
           for (int j = 0; j < n; ++j) {
-            tmpPosSum += board[i + 2][j];
+            tmpPosSum += current.board[i + 2][j];
           }
           if (i == 0) {
             for (int j = 0; j < n; ++j) {
-              tmpPosSum += board[i][j];
-              tmpPosSum += board[i + 1][j];
+              tmpPosSum += current.board[i][j];
+              tmpPosSum += current.board[i + 1][j];
             }
           }
         }
         if (tmpPosSum > maPosSum) {
           maPosSum = tmpPosSum;
           for (int t = nowCnt; t < cnt; ++t) {
-            for (int k = 0; k < 3; ++k) keepAns[t][k] = current_solution[t][k];
+            for (int k = 0; k < 3; ++k) keepAns[t][k] = current.solution[t][k];
           }
           for (int p = 0; p < n; ++p) {
             for (int q = 0; q < n; ++q) {
-              keepA[p][q] = board[p][q];
+              keepA[p][q] = current.board[p][q];
             }
           }
           maCntTail = cnt;
@@ -683,7 +678,7 @@ void Method4(double timeLimit)
         cnt = nowCnt;
         for (int p = 0; p < n; ++p) {
           for (int q = 0; q < n; ++q) {
-            board[p][q] = baseA[p][q];
+            current.board[p][q] = baseA[p][q];
           }
         }
       }
@@ -692,54 +687,50 @@ void Method4(double timeLimit)
 
       for (int t = nowCnt; t < maCntTail; ++t) {
         for (int k = 0; k < 3; ++k) {
-          current_solution[t][k] = keepAns[t][k];
+          current.solution[t][k] = keepAns[t][k];
         }
       }
       cnt = maCntTail;
       for (int p = 0; p < n; ++p) {
         for (int q = 0; q < n; ++q) {
-          board[p][q] = keepA[p][q];
+          current.board[p][q] = keepA[p][q];
         }
       }
     }
     if (ng) continue;
 
-    for (int t = cnt; t < T; ++t) current_solution[t][0] = -1;
-    current_score = CalcScore();
-    if (current_score > best_score) {
-      CopyToBest();
+    for (int t = cnt; t < T; ++t) current.solution[t][0] = -1;
+    current.score = CalcScore(current);
+    if (current.score > best.score) {
+      best = current;
     }
   }
 
-  if (mode != 0) cout << loopCount << endl;
+  current = best;
+
+  if (mode != 0) cout << "Method 4 : " << loopCount << endl;
 }
 
-ll Solve(int probNum)
+ll SolveCase(int probNum)
 {
   startTime = clock();
   endTime = clock();
 
-  SetUp();
+  State current = Input(probNum);
 
-  Input(probNum);
+  Initialize(current);
+  Method4(TL, current);
 
-  ofstream ofs;
-  OpenOfs(probNum, ofs);
+  Output(probNum, current);
 
-  Initialize();
-  Method4(TL);
-  CopyFromBest();
-
-  Output(ofs);
-
-  if (ofs.is_open()) {
-    ofs.close();
-  }
-
+  cerr << "Problem " << probNum << " solved." << endl;
   ll score = 0;
   if (mode != 0) {
-    score = CalcScore();
+    score = CalcScore(current);
+    cerr << "Score for problem " << probNum << ": " << score << endl;
   }
+
+  cerr << "Time taken for problem " << probNum << ": " << GetNowTime() << " seconds." << endl;
   return score;
 }
 
@@ -748,12 +739,13 @@ int main()
   mode = 1;
 
   if (mode == 0) {
-    Solve(0);
+    SolveCase(0);
   }
   else if (mode == 1) {
     ll sum = 0;
     for (int i = 0; i < 10; ++i) {
-      ll score = Solve(i);
+      ll score = SolveCase(i);
+      cerr << "Score for problem " << i << ": " << score << endl;
       sum += score;
       cout << "num = " << i << ", ";
       cout << "score = " << score << ", ";
