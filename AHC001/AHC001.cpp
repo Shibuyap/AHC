6a2200939cd11866cdf37f35fe15bb9f2b3ca116
Shibuyap <fumitaka.shibuya@gmail.com>
2025-06-23 03:13:05 +0900

    a

--- AHC001/AHC001.cpp
+++ AHC001/AHC001.cpp
@@ -21,6 +21,24 @@ typedef pair<int, int> P;
 
 #define MAX_N 205
 
+class Timer
+{
+private:
+  std::chrono::steady_clock::time_point start_time_clock;
+
+public:
+  void start()
+  {
+    start_time_clock = std::chrono::steady_clock::now();
+  }
+
+  double get_elapsed_time()
+  {
+    std::chrono::duration<double> elapsed = std::chrono::steady_clock::now() - start_time_clock;
+    return elapsed.count();
+  }
+};
+
 static uint32_t xorshift()
 {
   static uint32_t x = 123456789;
