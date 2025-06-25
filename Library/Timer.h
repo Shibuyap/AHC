bf1f55351c72a2e9cd162365e8374c0627b3238c
f-shibuya <f-shibuya@cgsys.co.jp>
2025-06-25 14:23:17 +0900

    a

---
+++ Library/Timer.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <chrono>
+
+class Timer
+{
+private:
+  std::chrono::steady_clock::time_point start_time_clock;
+
+public:
+  void start()
+  {
+    start_time_clock = std::chrono::steady_clock::now();
+  }
+
+  double get_elapsed_time()
+  {
+    std::chrono::duration<double> elapsed = std::chrono::steady_clock::now() - start_time_clock;
+    return elapsed.count();
+  }
+};
